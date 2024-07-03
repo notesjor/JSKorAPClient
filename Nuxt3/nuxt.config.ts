ff1c132b4be517dc7fa257686ee992ab8157d798
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2024-07-03 16:58:41 +0200

    // npm

--- Nuxt3/nuxt.config.ts
+++ Nuxt3/nuxt.config.ts
@@ -3,23 +3,29 @@ export default defineNuxtConfig({
   app: {
     baseURL: '/korapJS/'
   },
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   css: [
     'vuetify/lib/styles/main.sass',
     '@mdi/font/css/materialdesignicons.min.css',
     '~/assets/css/main.css'
   ],
+
   build: {
     transpile: ['vuetify'],
   },
+
   vite: {
     define: {
       'process.env.DEBUG': false,
     },
   },
+
+  compatibilityDate: '2024-07-03',
 })
